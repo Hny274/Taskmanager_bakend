865441937bc7cab6f8962276128906f15a695093
Hny274 <hunnygandhi274@gmail.com>
2023-11-08 13:55:08 +0530

    change in file

--- db/connect.js
+++ db/connect.js
@@ -1,6 +1,5 @@
 const mongoose=require('mongoose')
 
-//const connectionString='mongodb+srv://Hunny:hkah27731@taskmanager.sxc1gom.mongodb.net/Taskmanager?retryWrites=true&w=majority'
 
 const connectDB=(url)=>{
     return mongoose
